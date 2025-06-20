6dc0c8146e499dc7d6334fc93688207736672dcd
Vicky5_5 <vicgs99@gmail.com>
2025-06-20 23:11:59 +0200

    Agregar archivos de proyecto.

---
+++ Logica/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int idUsuario { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime fechaRegistro { get; set; }
+        public DateTime fechaBaja { get; set; }
+        public bool Administrador { get; set; }
+
+        public Usuario()
+        {
+            fechaRegistro = DateTime.Now;
+        }
+    }
+
+}
